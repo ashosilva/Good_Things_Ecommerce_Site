3b2f30397013fa6c02e58b6703c4408413463786
Jacob Chen <jacobpchen@gmail.com>
2021-03-31 21:23:10 -0400

    implemented creating new order, order schema

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,13 @@ app.use(cookieParser())
 // Import all routes
 const products = require('./routes/product')
 const auth = require('./routes/auth')
+const order = require('./routes/order')
 
 app.use('/api/v1', products)
 app.use('/api/v1', auth)
+app.use('/api/v1', order)
 
 // Middleware to handle errors
 app.use(errorMiddleware)
-
 
 module.exports = app
