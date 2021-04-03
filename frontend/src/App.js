03372d8f1329c5af423ff495b1e43ff82753bee5
Jacob Chen <jacobpchen@gmail.com>
2021-04-02 21:20:02 -0400

    Created a home page with a logo, dummy searchbar, and a static item

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
