d82446c28ae9ab6d86992fa58be3c1752f9e6824
Abayomi Shosilva <ashosilva@gmail.com>
2021-04-07 16:34:46 -0400

    about page

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { NavLink } from "react-router-dom"
 
 const Footer = () => {
     return (
@@ -7,6 +8,9 @@ const Footer = () => {
                 <p className="text-center mt-1">
                     Good Things 2021
                  </p>
+                <NavLink to="/about" exact className="flex justify-center text-yellow-700 hover:text-gray-500 hover:underline">
+                    About Us
+                </NavLink>
             </footer>
         </Fragment>
     )
