a3753343a02f646ffa49aef70cd8c0f8eae7805c
Jacob Chen <jacobpchen@gmail.com>
2021-05-15 21:34:42 -0400

    implemented new product component

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,10 +3,34 @@ const Product = require('../models/products')
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 const APIFeatures = require('../utils/apiFeatures')
+const cloudinary = require('cloudinary')
 
 // Create new product =>  / api/v1/admin/product/
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
+    let images = []
+    // if user uploads one image it is a string, else it's an array
+    if (typeof req.body.images === 'string') {
+        images.push(req.body.images)
+    } else {
+        images = req.body.images
+    }
+
+    let imagesLinks = []
+
+    for (let i = 0; i < images.length; i++) {
+        // upload each picture to cloudinary
+        const result = await cloudinary.v2.uploader.upload(images[i], {
+            folder: 'products'
+        })
+
+        imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        })
+    }
+
+    req.body.images = imagesLinks
     req.body.user = req.user.id
 
     // Get the req.body and save it as product
@@ -29,7 +53,7 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
     const apiFeatures = new APIFeatures(Product.find(), req.query)
         .search()
         .filter()
-        
+
     let products = await apiFeatures.query
     let filteredProductsCount = products.length
 
@@ -184,4 +208,16 @@ exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         success: true
     })
+})
+
+// Get all products (Admin)  =>   /api/v1/admin/products
+exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
+
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+
 })
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const { 
-    getProducts, 
+const {
+    getProducts,
+    getAdminProducts,
     newProduct,
     getSingleProduct,
-    updateProduct, 
+    updateProduct,
     deleteProduct,
     createProductReview,
     getProductReviews,
     deleteReview
-    } = require('../controllers/productController')
+} = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth')
 
@@ -26,8 +27,9 @@ router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 // Admin 
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
+router.route('/admin/products').get(getAdminProducts);
 router.route('/admin/product/:id')
-    .put(isAuthenticatedUser, authorizeRoles('admin'),  updateProduct)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
 
 module.exports = router
